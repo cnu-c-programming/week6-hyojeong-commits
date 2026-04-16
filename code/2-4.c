164f788dbc332f9c6fb17d0ec5e7bba34e6a7bc3
hyojeong-commits <kweunsungo@gmail.com>
2026-04-16 23:38:40 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -3,7 +3,7 @@
 void print_2d_arr(int* rows[], int row_size, int col_size){
     for (int i=0; i<row_size; i++){
         for(int j=0; j<col_size; j++){
-            printf("%d", rows[i][j]);
+            printf("%d ", rows[i][j]);
         }
         printf("\n");
     }
